c904af213a48f18fbb7dab4e56cccbc1120547bd
malakelhalawany06 <yousrymalak79@gmail.com>
2026-04-09 13:08:35 +0200

    to-do list (not complete)

--- to-dolist.js
+++ to-dolist.js
@@ -1,14 +1,21 @@
-function checkOrUncheck(){
-        
-        const check=document.querySelector('.task-check');
-        const task=document.querySeelector('.task-name')
-        if(check.classList.contains('done') && task.classList.contains('done')){
-        check.classList.remove('done');
-        task.classList.remove('done');
-        }
-        else {
-            check.classList.add('done');
-            task.classList.add('done');
-        }
+function checkOrUncheck(button) {
+  const taskItem = button.closest('.task-item');
+  const check = taskItem.querySelector('.task-check');
+  const task = taskItem.querySelector('.task-name');
+  
+  check.checked = !check.checked;
+  
+  if (check.checked) {
+    check.classList.add('done');
+    check.innerHTML = "✓";
+    task.classList.add('done');
+  } else {
+    check.classList.remove('done');
+    check.innerHTML = "";
+    task.classList.remove('done');
+  }
+}
 
-    }
+function addTask(){
+    
+}
